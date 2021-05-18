6e9bf6137a668b60e85cc62d3b8870a2b5ac58f5
jgutierrez12 <70130916+jgutierrez12@users.noreply.github.com>
2021-05-18 18:42:14 +0200

    Update run.sh
    VLC changes

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
-ffmpeg -rtsp_transport tcp \
-    -i "rtsp://admin:5219E4766d@192.168.1.88:554/H264&ch=1&subtype=0" \
-    -i "rtsp://admin:5219E4766d@192.168.1.88:554/H264&ch=2&subtype=0" \
-    -filter_complex "
-        nullsrc=size=960x1080 [base];
-        [0:v] setpts=PTS-STARTPTS, scale=960x540 [upperleft];
-        [1:v] setpts=PTS-STARTPTS, scale=960x540 [lowerleft];
-        [base][upperleft] overlay=shortest=1 [tmp1];
-        [tmp1][lowerleft] overlay=shortest=1:y=540
-      " \
-	-c:v libx264 -preset superfast -crf 18 -r 500/21 -f matroska - | ffplay -
+# ffmpeg -rtsp_transport tcp \
+#     -i "rtsp://admin:5219E4766d@192.168.1.88:554/H264&ch=1&subtype=0" \
+#    -i "rtsp://admin:5219E4766d@192.168.1.88:554/H264&ch=2&subtype=0" \
+#    -filter_complex "
+#        nullsrc=size=960x1080 [base];
+#        [0:v] setpts=PTS-STARTPTS, scale=960x540 [upperleft];
+#        [1:v] setpts=PTS-STARTPTS, scale=960x540 [lowerleft];
+#        [base][upperleft] overlay=shortest=1 [tmp1];
+#        [tmp1][lowerleft] overlay=shortest=1:y=540
+#      " \
+#	-c:v libx264 -preset superfast -crf 18 -r 500/21 -f matroska - | ffplay -
+
+
+vlc --vlm-conf lacova.vlm.conf --mosaic-width=960 --mosaic-height=1080 --image-duration=-1 --mosaic-keep-aspect-ratio --mosaic-keep-picture
